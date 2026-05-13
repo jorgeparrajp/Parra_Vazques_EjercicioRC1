0cbebf111b8dce5a65b1063e6fa00c3ad73e2fbc
Jorge Parra <jorgeenriqueparrachora@gmail.com>
2026-05-13 14:39:47 -0500

    Actualiza archivos de punteros

---
+++ ejercicio2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+char vocales [] = {'a',' e','i',' o',' u'};
+
+for (int i = 0 ; i < 5 ; i ++ ){
+
+    printf ("vocales[%d] %c \n ", i, vocales [i]);
+}
+return 0; 
+}
