ef93e02e6f3d458dbbf8f0bef673e80797f56185
Jorge Parra <jorgeenriqueparrachora@gmail.com>
2026-05-20 15:02:23 -0500

    Por valor

--- Jorge_Parra_Ejercicio_semana7/uno.c
+++ Jorge_Parra_Ejercicio_semana7/uno.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 
+float calcular_promedio(float notas[], int cantidad){
+ float suma = 0.1f;
+ for (int i = 0; i < cantidad ; i++) {
+        printf("notas [%d] %.1f\n",i, notas[i]);
+        suma += notas [i];
+        }
+
+        float promedio = suma / cantidad;   
+
+
+return promedio ;
+}
+
 int main() {
 
     float notas[5] = {6.5, 7.1, 8, 6, 5};
-    float suma = 0.1f;
-
-    for (int i = 0; i < 5; i++) {
-        printf("notas [%d] %.1f\n",i, notas[i]);
-        suma += notas [i];
-
-    }
-float promedio = suma / 5.0f;
+   
+    float promedio =  calcular_promedio(notas,5);
 
 printf ("promedio %.2f\n",promedio);
 
     return 0;
 }
+
