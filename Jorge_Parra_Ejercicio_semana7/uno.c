afe80065ddd4d0e87125160c1c818aafb18cda67
Jorge Parra <jorgeenriqueparrachora@gmail.com>
2026-05-20 15:10:49 -0500

    funcion calclar el promedio por referencia

--- Jorge_Parra_Ejercicio_semana7/uno.c
+++ Jorge_Parra_Ejercicio_semana7/uno.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-float calcular_promedio(float notas[], int cantidad){
+float calcular_promedio(float notas [5]){
+    float calcular_promedio(float *ptr);
  float suma = 0.1f;
- for (int i = 0; i < cantidad ; i++) {
+ for (int i = 0; i < 5 ; i++) {
         printf("notas [%d] %.1f\n",i, notas[i]);
         suma += notas [i];
         }
 
-        float promedio = suma / cantidad;   
+        float promedio = suma / 5 ;   
 
 
 return promedio ;
@@ -17,7 +18,7 @@ int main() {
 
     float notas[5] = {6.5, 7.1, 8, 6, 5};
    
-    float promedio =  calcular_promedio(notas,5);
+    float promedio =  calcular_promedio(notas);
 
 printf ("promedio %.2f\n",promedio);
 
