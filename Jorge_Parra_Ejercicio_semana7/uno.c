1417fc637baebc1d783f7bd2a1cc8511c864e332
Jorge Parra <jorgeenriqueparrachora@gmail.com>
2026-05-20 14:43:08 -0500

    Segundo commit

--- Jorge_Parra_Ejercicio_semana7/uno.c
+++ Jorge_Parra_Ejercicio_semana7/uno.c
@@ -3,10 +3,16 @@
 int main() {
 
     float notas[5] = {6.5, 7.1, 8, 6, 5};
+    float suma = 0.1f;
 
     for (int i = 0; i < 5; i++) {
         printf("notas [%d] %.1f\n",i, notas[i]);
+        suma += notas [i];
+
     }
+float promedio = suma / 5.0f;
+
+printf ("promedio %.2f\n",promedio);
 
     return 0;
 }
